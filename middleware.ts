1dfbdf12df832fc5818aeaa1aec7fcddf5d3b7da
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-02-10 11:05:58 -0500

    jnfkjneo

--- middleware.ts
+++ middleware.ts
@@ -1,35 +1,15 @@
-import { defineMiddleware } from "astro/middleware";
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+  const { pathname, origin } = url;
 
-// export default function middleware(request: Request) {
-//   const url = new URL(request.url);
-//   const { pathname, origin } = url;
+  if (pathname === "/")
+    return Response.redirect(new URL("/posts", origin), 302);
 
-//   if (pathname === "/")
-//     return Response.redirect(new URL("/posts", origin), 302);
-
-//   return new Response(request.url);
-//   //   const url = new URL(request.url);
-//   //   // You can retrieve IP location or cookies here.
-//   //   if (url.pathname === "/admin") {
-//   //     url.pathname = "/";
-//   //   }
-//   //   return Response.redirect(url);
-// }
-
-const onRequest = defineMiddleware((context, next) => {
-  //   if (context.url.pathname === INDEX_PATH) {
-  //     // return Response.redirect(new URL("/redirected", context.url), 302);
-  //     return new Response(
-  //       JSON.stringify({
-  //         message: new URL("/posts", context.url),
-  //       }),
-  //       {
-  //         status: 200,
-  //       }
-  //     );
+  //   return new Response(request.url);
+  //   const url = new URL(request.url);
+  //   // You can retrieve IP location or cookies here.
+  //   if (url.pathname === "/admin") {
+  //     url.pathname = "/";
   //   }
-
-  return next();
-});
-
-export default onRequest;
+  //   return Response.redirect(url);
+}
