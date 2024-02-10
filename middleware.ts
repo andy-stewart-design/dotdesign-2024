2a7acf2108abca1c1763797420517b8e24292fb5
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-02-10 11:10:35 -0500

    Switching to using middleware in the root dir

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,4 @@ export default function middleware(request: Request) {
 
   if (pathname === "/")
     return Response.redirect(new URL("/posts", origin), 302);
-
-  //   return new Response(request.url);
-  //   const url = new URL(request.url);
-  //   // You can retrieve IP location or cookies here.
-  //   if (url.pathname === "/admin") {
-  //     url.pathname = "/";
-  //   }
-  //   return Response.redirect(url);
 }
