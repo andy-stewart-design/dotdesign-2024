92894fc6500ca8b63291cf334f769334ab948ea4
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-02-10 09:11:39 -0500

    middleware test 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,15 @@ export const onRequest = defineMiddleware((context, next) => {
   //   else return next();
   console.log(context.url.pathname);
 
+  return new Response(
+    JSON.stringify({
+      message: "Hello world",
+    }),
+    {
+      status: 200,
+    }
+  );
+
   if (context.url.pathname === "/")
     return Response.redirect(new URL("/posts", context.url), 302);
   else return next();
