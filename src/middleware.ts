3cb539313c755f55c3242013d9ca94bcec98a5cb
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-02-10 09:20:11 -0500

    testing cache purge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,6 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  //   const slug = context.url.pathname.replace(/\/$/gm, "").split("/").at(-1);
-
-  //   if (slug === "")
-  //     return Response.redirect(new URL("/posts", context.url), 302);
-  //   else return next();
   console.log(context.url.pathname);
 
   if (context.url.pathname === "/")
