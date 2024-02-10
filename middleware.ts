bce911f6b146990e5a81f7f8ff64c8677e75b231
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-02-10 11:01:21 -0500

    redirect/response test

--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,7 @@ export default function middleware(request: Request) {
   if (pathname === "/")
     return Response.redirect(new URL("/posts", origin), 302);
 
-  return new Response(
-    JSON.stringify({
-      url: request.url,
-      path: url.pathname,
-      origin: url.origin,
-    }),
-    {
-      status: 200,
-    }
-  );
+  return new Response(request.url);
   //   const url = new URL(request.url);
   //   // You can retrieve IP location or cookies here.
   //   if (url.pathname === "/admin") {
