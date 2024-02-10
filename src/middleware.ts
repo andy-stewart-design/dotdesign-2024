8cb8122d256670faea55ecb324139b14ced22e85
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-02-10 10:33:36 -0500

    fixing dumb error

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ const BASE_URL = "https://dotdesign-2024.vercel.app";
 
 export const onRequest = defineMiddleware((context, next) => {
   if (context.url.pathname === INDEX_PATH) {
-    return Response.redirect(new URL("/redirected", BASE_URL), 302);
+    return Response.redirect(new URL("/posts", BASE_URL), 302);
   }
 
   return next();
