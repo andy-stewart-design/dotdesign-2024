0db8c99608f8932a2bc9933ffec5d86ebc43a420
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-02-11 12:32:36 -0500

    alert update

--- src/components/global/AlertBar/AlertBar.tsx
+++ src/components/global/AlertBar/AlertBar.tsx
@@ -4,8 +4,9 @@ export default function AlertBar() {
   return (
     <div class={classes.alert}>
       <p>
-        <b>Bless the mess!</b> This site is being built as you read it. LMK if
-        something looks weird.
+        <b>
+          This site is being built as you read it. LMK if something looks weird.
+        </b>
       </p>
     </div>
   );
